f4e26faa7a9fd2c927716b5d52c485df35439dac
KonkovaAnna <121241043+KonkovaAnna@users.noreply.github.com>
2023-02-03 20:03:01 +0300

    [add] modified 6

--- CSlight/Practice_6/Practice_6/Program.cs
+++ CSlight/Practice_6/Practice_6/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Practice_6
@@ -12,19 +10,57 @@ namespace Practice_6
 
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+
             Console.OutputEncoding = System.Text.Encoding.UTF8;// Для корректной кодировки вывода в консоль ( без этой строки все сиволы "?" )
+
             char[,] map = ReadMap("map.txt");
-            DrawMap(map);
+            ConsoleKeyInfo pressedKey = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
+
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    pressedKey = Console.ReadKey();
+                }
+
+            });
+
+            int pacmanX = 1;
+            int pacmanY = 1;
+            int score = 0;
+
+            while (true)
+            {
+                Console.Clear();
+
+                HandleInput(pressedKey, ref pacmanX, ref pacmanY, map, ref score);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                DrawMap(map);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(pacmanX, pacmanY);
+                Console.WriteLine("@");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.SetCursorPosition(24, 0);
+                Console.WriteLine($"Score: {score}");
+
+                
+            }
+
+            Console.ReadLine();
         }
 
-         private static char[,] ReadMap(string path)
+        private static char[,] ReadMap(string path)
         {
-            string[] file=File.ReadAllLines("map.txt");
+            string[] file = File.ReadAllLines("map.txt");
 
             char[,] map = new char[GetMaxLengthOfline(file), file.Length];
 
-            for(int x =0; x<map.GetLength(0); x++)
-                for(int y = 0; y < map.GetLength(0); y++)
+            for (int x = 0; x < map.GetLength(0); x++)
+                for (int y = 0; y < map.GetLength(1); y++)
                     map[x, y] = file[y][x];
 
             return map;
@@ -32,26 +68,66 @@ namespace Practice_6
 
         private static void DrawMap(char[,] map)
         {
-            for (int y = 0; y < map.GetLength(0); y++)
+            for (int y = 0; y < map.GetLength(1); y++)
             {
                 for (int x = 0; x < map.GetLength(0); x++)
-
                 {
                     Console.Write(map[x, y]);
                 }
                 Console.Write("\n");
             }
-                
-                    
+
+
         }
+
+        private static void HandleInput(ConsoleKeyInfo pressedKey, ref int pacmanX, ref int pacmanY, char[,] map, ref int score)
+        {
+            int[] direction = GetDirection(pressedKey);
+
+            int nextPacmanPositionX = pacmanX + direction[0];
+            int nextPacmanPositionY = pacmanY + direction[1];
+
+            char nextCell = map[nextPacmanPositionX, nextPacmanPositionY];
+
+            if (nextCell == ' ' || nextCell=='.')
+            {
+                pacmanX = nextPacmanPositionX;
+                pacmanY = nextPacmanPositionY;
+
+                if (nextCell == '.')
+                {
+                    score++;
+                    map[nextPacmanPositionX, nextPacmanPositionY] = ' ';
+                }
+            }
+        }
+        private static int[] GetDirection(ConsoleKeyInfo pressedKey)
+        {
+            int[] direction = { 0, 0 };
+
+            if (pressedKey.Key == ConsoleKey.UpArrow)
+                direction[1] -= 1;
+            else if (pressedKey.Key == ConsoleKey.DownArrow)
+                direction[1] = 1;
+            else if (pressedKey.Key == ConsoleKey.LeftArrow)
+                direction[0] -= 1;
+            else if (pressedKey.Key == ConsoleKey.RightArrow)
+                direction[0] = 1;
+
+            return direction;
+        }
+
+
         private static int GetMaxLengthOfline(string[] lines)
         {
             int maxLength = lines[0].Length;
 
-            foreach(var line in lines)
-                if(line.Length>maxLength)
-                    maxLength= line.Length;
+            foreach (var line in lines)
+                if (line.Length > maxLength)
+                    maxLength = line.Length;
             return maxLength;
         }
+
+
     }
 }
