274b300d870fd5331f06980b498e3d5edc27d0e9
KonkovaAnna <121241043+KonkovaAnna@users.noreply.github.com>
2023-02-09 01:50:21 +0300

    [add]exe_2

---
+++ Practice_3/Practice_exe2/Practice_exe2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[][] arr = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new int[n];
+            }
+
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i][j] = random.Next(0, 2);
+
+                    if (arr[i][j] == 1)
+                    {
+                        Console.WriteLine($"The element is found and what is its index: [{ i},{ j}]");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
