9987179d7d9108212eaeb702120ebcd9174d120d
KonkovaAnna <121241043+KonkovaAnna@users.noreply.github.com>
2023-02-26 23:45:49 +0300

    [add]task4 (two-dimensional array generator)

--- Practice_3/Practice_exe4/Practice_exe4/Program.cs
+++ Practice_3/Practice_exe4/Practice_exe4/Program.cs
@@ -10,7 +10,21 @@ namespace Practice_exe4
     {
         static void Main(string[] args)
         {
+            int r = 4, c = 3;
+            int[,] array = new int[r, c];
+            Random random = new Random();
 
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    array[i,j] = random.Next(0, 50);
+                    Console.Write($" {array[i, j]}\t");
+                }
+                Console.WriteLine("");
+            }
+            
+            Console.ReadLine();
         }
     }
 }
